e9d58a83f6e73516689dbed7b5114cac2b4012e4
MelanieBovenzo <melaniebovenzo@gmail.com>
2026-09-09 11:52:27 -0300

    Mochila Take 3

--- Devil Kitty/Assets/Scripts/UI/BackpackController.cs
+++ Devil Kitty/Assets/Scripts/UI/BackpackController.cs
@@ -1,10 +1,14 @@
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class BackpackController : MonoBehaviour
 {
     [SerializeField] GameObject itemObject;
     private List<int> createdItems = new List<int>();
+    int itemCount;
 
     [Header("References")]
     [SerializeField] ItemManager itemManager;
@@ -24,55 +28,69 @@ public class BackpackController : MonoBehaviour
 
     public void OpenBackpack()
     {
+        createdItems.Clear();
+        foreach (Transform child in backpackPanel.transform)
+        {
+            Destroy(child.gameObject);
+        }
         foreach (int item in inv.inventoryTags)
         {
             Vector3 itemPosition;
-            if (createdItems.Contains(item))
-            {
 
-            }
-            else
-            {
+            if (!createdItems.Contains(item)) {
+                
                 switch (createdItems.Count)
                 {
                     // DEFINE ITEM POSITION IN BACKPACK
                     case 0:
-                        itemPosition = new Vector3(-200,115,0);
+                        itemPosition = new Vector3(-200,125,0);
                         break;
                     case 1:
-                        itemPosition = new Vector3(0, 115, 0);
+                        itemPosition = new Vector3(0, 125, 0);
                         break;
                     case 2:
-                        itemPosition = new Vector3(200, 115, 0);
+                        itemPosition = new Vector3(200, 125, 0);
                         break;
                     case 3:
-                        itemPosition = new Vector3(-200, 0, 0);
+                        itemPosition = new Vector3(-200, -20, 0);
                         break;
                     case 4:
-                        itemPosition = new Vector3(0, 0, 0);
+                        itemPosition = new Vector3(0, -20, 0);
                         break;
                     case 5:
-                        itemPosition = new Vector3(200, 0, 0);
+                        itemPosition = new Vector3(200, -20, 0);
                         break;
                     case 6:
-                        itemPosition = new Vector3(-200, -115, 0);
+                        itemPosition = new Vector3(-200, -145, 0);
                         break;
                     case 7:
-                        itemPosition = new Vector3(0, -115, 0);
+                        itemPosition = new Vector3(0, -145, 0);
                         break;
                     case 8:
-                        itemPosition = new Vector3(200, -115, 0);
+                        itemPosition = new Vector3(200, -145, 0);
                         break;
                     default:
                         itemPosition = new Vector3(0,0,0);
                         break;
                 }
-                createdItems.Add(item);
+                
                 GameObject createdItem = Instantiate(itemObject, itemPosition, new Quaternion(0, 0, 0, 1), backpackPanel);
                 createdItem.transform.localPosition = itemPosition;
                 BackpackItem backpackItem = createdItem.GetComponent<BackpackItem>();
-
                 backpackItem.itemTag = item;
+
+                // GETS THE ITEM COUNT FROM INVENTORY
+                itemCount = 0;
+                foreach (int itemTaggy in inv.inventoryTags)
+                {
+                    if (itemTaggy == item)
+                    {
+                        ++itemCount;
+                    }
+                }
+                backpackItem.itemQuantity = itemCount;
+                
+
                 switch (item)
                 {
                     case 1:
@@ -90,7 +108,14 @@ public class BackpackController : MonoBehaviour
                     case 5:
                         backpackItem.itemImage = itemManager.item5Sprite;
                         break;
+                    case 6:
+                        backpackItem.itemImage = itemManager.item6Sprite;
+                        break;
+                    case 7:
+                        backpackItem.itemImage = itemManager.item7Sprite;
+                        break;
                 }
+                createdItems.Add(item);
             }
         }
     }
--- Devil Kitty/Assets/Scripts/UI/BackpackItem.cs
+++ Devil Kitty/Assets/Scripts/UI/BackpackItem.cs
@@ -5,9 +5,11 @@ using UnityEngine.UI;
 public class BackpackItem : MonoBehaviour
 {
     public int itemTag;
+    public int itemQuantity;
 
     [SerializeField] TextMeshProUGUI nameText;
     [SerializeField] Image imageComponent;
+    [SerializeField] TextMeshProUGUI quantityText;
 
     public Sprite itemImage;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -22,7 +24,7 @@ public class BackpackItem : MonoBehaviour
                 nameText.text = "Pistola de laser";
                 break;
             case 3:
-                nameText.text = "+1 dano";
+                nameText.text = "+ Dano";
                 break;
             case 4:
                 nameText.text = "Lança Doce";
@@ -30,11 +32,18 @@ public class BackpackItem : MonoBehaviour
             case 5:
                 nameText.text = "Martelo de Pirulito";
                 break;
+            case 6:
+                nameText.text = "+ Vida";
+                break;
+            case 7:
+                nameText.text = "+ Velocidade de Movimento";
+                break;
             default:
                 nameText.text = "ERRO!";
                 break;
         }
         imageComponent.sprite = itemImage;
+        quantityText.text = itemQuantity.ToString();
     }
 
     // Update is called once per frame
